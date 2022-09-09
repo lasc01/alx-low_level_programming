e24e98f70fba978aca25e988e9f21ad34a5415c0
lasc01 <olamideabioro@gmail.com>
2022-09-09 06:56:30 -0700

    commit for task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the lowercase alphabet in reverse,
+ * followed by a new line.
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+
+	char cr;
+	for (cr = 'z'; cr >= 'a'; cr--)
+	{
+		putchar(cr);
+	}
+	putchar('\n');
+	return (0);
+}
+
