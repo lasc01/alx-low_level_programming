af6bfae7a8ba9d07a9b66a93168706921490ee87
lasc01 <olamideabioro@gmail.com>
2022-09-09 06:36:06 -0700

    commit for task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints all single digit numbers of base
+ * 10 starting from 0, followed by a new line.
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int n;
+	for(n = 0; n < 10; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
