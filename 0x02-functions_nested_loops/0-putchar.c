c1d917088a90532d6b5be4244cd9d7db8844bb7b
lasc01 <olamideabioro@gmail.com>
2022-09-13 05:51:42 -0700

    third commit for task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,23 @@
 #include "main.h"
 
 /**
- * main - program that prints _putchar
- * @c: Print _putchar
+ * main - prints _putchar
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
  */
+
 int main(void)
 {
-	char my_put[] = "_putchar";
-        int i = 0;
+	char text[9] = "_putchar";
+	int i = 0;
 
-        for (i = 0; i <= 7; i++)
+	for (i = 0; i < 8; i++)
 	{
-                _putchar(my_put[i]);
-        }
-        _putchar('\n');
+		_putchar(text[i]);
+	}
+	_putchar('\n');
 
-        return (0);
+	return (0);
 }
