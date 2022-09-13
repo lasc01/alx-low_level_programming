fe6c24e20f7ffc2ea6abdca3ca1d12d4920b9794
lasc01 <olamideabioro@gmail.com>
2022-09-13 05:46:50 -0700

    second commit for task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,13 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-
 int main(void)
 {
-        char my_put[] = "_putchar";
+	char my_put[] = "_putchar";
         int i = 0;
 
-        for (i = 0; i <= 7; i++){
+        for (i = 0; i <= 7; i++)
+	{
                 _putchar(my_put[i]);
         }
         _putchar('\n');
