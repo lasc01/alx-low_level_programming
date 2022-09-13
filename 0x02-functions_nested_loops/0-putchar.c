6c8893cf1fc2c52da942e66428eb2355398fedaa
lasc01 <olamideabioro@gmail.com>
2022-09-13 05:39:53 -0700

    my commit for task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - program that prints _putchar
+ * @c: Print _putchar
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int main(void)
+{
+        char my_put[] = "_putchar";
+        int i = 0;
+
+        for (i = 0; i <= 7; i++){
+                _putchar(my_put[i]);
+        }
+        _putchar('\n');
+
+        return (0);
+}
