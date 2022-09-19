a631e21b3aedce7733f42b361fd00b05d8ea654a
lasc01 <olamideabioro@gmail.com>
2022-09-19 09:44:34 -0700

    correction 2 for task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,30 +1,24 @@
 #include "main.h"
 
 /**
- * rev_string - function that prints a string, in reverse,
- * followed by a new line.
+ * print_rev - prints string backwards
  * @s: string
  * Return: void
  */
 
-void rev_string(char *s)
+void print_rev(char *s)
 {
-	int i, max, mid;
-	char first, last;
+	int i = 0;
 
-	i = 0;
 	while (s[i] != '\0')
 	{
 		i++;
 	}
-	max = i - 1;
-	mid = max / 2;
-	while (mid >= 0)
+	i--;
+	while (i >= 0)
 	{
-		first = s[max - mid];
-		last = s[mid];
-		s[mid] = first;
-		s[max - mid] = last;
-		mid--;
+		_putchar(s[i]);
+		i--;
 	}
+	_putchar('\n');
 }
