222332e5062858a6f8145ef2f8fda30effcc17d3
lasc01 <olamideabioro@gmail.com>
2022-09-19 09:37:44 -0700

    commit for task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - function that prints a string, in reverse,
+ * followed by a new line.
+ * @s: string
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i, max, mid;
+	char first, last;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	max = i - 1;
+	mid = max / 2;
+	while (mid >= 0)
+	{
+		first = s[max - mid];
+		last = s[mid];
+		s[mid] = first;
+		s[max - mid] = last;
+		mid--;
+	}
+}
