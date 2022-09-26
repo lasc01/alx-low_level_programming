499f6de0a9cca156e04939036736ec166c3a0762
lasc01 <olamideabioro@gmail.com>
2022-09-26 09:06:16 -0700

    correction 3 for task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _memcpy - function copies n bytes 
+ * _memcpy -  function copies n
  * @dest: pointer to char params
  * @src: pointer to char params
  * @n: size
@@ -11,7 +11,7 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	
+
 	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
