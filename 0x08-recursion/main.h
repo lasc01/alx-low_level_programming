5291f20093beb384f3ceb5a93ff3271a9ed13888
lasc01 <olamideabioro@gmail.com>
2022-09-28 09:17:36 -0700

    commit for task 6

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,4 +5,7 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int _sqrt(int n, int i);
+int is_prime_number(int n);
+int divisors(int n, int m);
+int is_prime_number(int n);
+
