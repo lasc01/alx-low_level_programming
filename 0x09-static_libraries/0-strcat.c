55f212681f008b3fa4b4f2d636f4df33d8f4beee
lasc01 <olamideabioro@gmail.com>
2022-09-30 15:37:29 -0700

    commit for task 0

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,14 +1,24 @@
 #include "main.h"
+
 /**
-*_isdigit - function that verifies if a  character is a digit or not
-*@c: tested character
-*Return: returns 1 if it is uppercase , 0 if not
-*/
+ *_strcat - concatenates  the string pointed to by @src to
+ * the end of the string pointed to by @dest
+ *@dest: String that will be appended
+ *@src: String to be concatenated upon
+ *
+ * Return: returns poiner to @dest
+ */
 
-int _isdigit(int c)
+char *_strcat(char *dest, char *src)
 {
-if ((c >= 48) && (c <= 57))
-return (1);
 
-return (0);
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
 }
